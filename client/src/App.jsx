f08b39ef0cbef19652a74ebd04a1cde0b8c41255
bhoomi580 <bhoomikav483@gmail.com>
2025-09-10 19:15:10 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ChatWindow from "./components/ChatWindow";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>AI Chatbot 🤖</h1>
+      <ChatWindow />
+    </div>
+  );
+}
